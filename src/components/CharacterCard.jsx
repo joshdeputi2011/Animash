a9a57fadde9c8deeac7ea9d95bcc103da91221f9
Josh Saju George <joshsaju2011@gmail.com>
2026-02-08 02:28:10 +0530

    Delete src directory

--- src/components/CharacterCard.jsx
+++
@@ -1,8 +0,0 @@
-export default function CharacterCard({ character, onVote }) {
-  return (
-    <div className="card" onClick={() => onVote(character.id)}>
-      <img src={character.image} alt={character.name} />
-      <h3>{character.name}</h3>
-    </div>
-  );
-}
